e151d157079277db75739a916c58454d5ee38b70
Quinn B <43831604+Qbolewicki@users.noreply.github.com>
2020-08-28 15:02:50 -0400

    Tutorial 1 final hovercat

---
+++ tutorial0_serve/main.js
@@ -0,0 +1,16 @@
+function myMove() {
+    var elem = document.getElementById("animate");   
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 400) {
+        clearInterval(id);
+      } else {
+        pos++;
+        elem.style.top = pos + "px"; 
+        elem.style.bottom = pos + "px"; 
+        elem.style.left = pos + "px"; 
+        elem.style.right = pos + "px";
+      }
+    }
+  }
