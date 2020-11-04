1146e4d321ad98ee1041cf40ab12767a97cb972c
Quinn B <43831604+Qbolewicki@users.noreply.github.com>
2020-11-04 18:25:57 -0500

    desktop

--- Womens_project/main.js
+++ Womens_project/main.js
@@ -2,10 +2,11 @@
 * CONSTANTS AND GLOBALS 
 * */
 const width = window.innerWidth * 0.9,
-  height = window.innerHeight / 3,
+height = window.innerHeight * 0.7,
   paddingInner = 0.2,
-  margin = { top: 20, bottom: 40, left: 40, right: 40 };
+  margin = { top: 20, bottom: 50, left: 40, right: 40 };
   default_selection = "Select a Service";
+
 let svg;
 let xScale;
 let yScale;
@@ -29,79 +30,88 @@ init();
 
 function init() {
 
-/** SCALES */
-// reference for d3.scales: https://github.com/d3/d3-scale
-const xScale = d3
-  .scaleBand()
-  .domain(data.map(d => d.Borough))
-  .range([margin.left, width - margin.right])
-  .paddingInner(paddingInner);
-
-const yScale = d3
-  .scaleLinear()
-  .domain([0, d3.max(data, d => d.OrganizationNumber)])
-  .range([height - margin.bottom, margin.top]);
-
-const xAxis = d3.axisBottom(xScale);
-const yAxis = d3.axisLeft(yScale);
-
-// UI ELEMENT SETUP
-// add dropdown (HTML selection) for interaction
-// HTML select reference: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select
-const selectElement = d3.select("#dropdown").on("change", function() {
-  console.log("new selected Focus is", this.value);
-  // `this` === the selectElement
-  // this.value holds the dropdown value a user just selected
-  focus1.selectedFocus = this.value;
-  draw(); // re-draw the graph based on this new selection
-});
-
-// add in dropdown options from the unique values in the data
-selectElement
-  .selectAll("option")
-  .data([
-    ...Array.from(new Set(focus1.data.map(d => d.focus1))),
-    default_selection,
-  ])
-  .join("option")
-  .attr("value", d => d)
-  .text(d => d);
-// this ensures that the selected value is the same as what we have in state when we initialize the options
-selectElement.property("value", default_selection);
-
-/** MAIN CODE */
-svg = d3
-  .select("#d3-container")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height);
-
-  // add the xAxis
-svg
-  .append("g")
-  .attr("class", "axis x-axis")
-  .attr("transform", `translate(0,${height - margin.bottom})`)
-  .call(xAxis)
-  .append("text")
-  .attr("class", "axis-label")
-  .attr("x", "50%")
-  .attr("dy", "3em")
-  .text("Number of Protesters");
-// add the yAxis
-svg
-  .append("g")
-  .attr("class", "axis y-axis")
-  .attr("transform", `translate(${margin.left},0)`)
-  .call(yAxis)
-  .append("text")
-  .attr("class", "axis-label")
-  .attr("y", "50%")
-  .attr("dx", "-3em")
-  .attr("writing-mode", "vertical-rl")
-  .text("State Response");
-draw(); // calls the draw function
+  /** SCALES */
+  // reference for d3.scales: https://github.com/d3/d3-scale
+  xScale = d3
+    .scaleBand()
+    .domain(d3.extent(focus1.data, d => d.Borough))
+    .range([margin.left, width - margin.right])
+    .paddingInner(paddingInner);
+  yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(focus1.data,d => d.OrganizationNumber))
+    .range([height - margin.bottom, margin.top]);
+  
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+  
+  // UI ELEMENT SETUP
+  // add dropdown (HTML selection) for interaction
+  // HTML select reference: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select
+  const selectElement = d3.select("#dropdown").on("change", function() {
+    console.log("new selected Focus is", this.value);
+    // `this` === the selectElement
+    // this.value holds the dropdown value a user just selected
+    focus1.selectedFocus = this.value;
+    draw(); // re-draw the graph based on this new selection
+  });
+  
+  // add in dropdown options from the unique values in the data
+  selectElement
+    .selectAll("option")
+    .data([
+      ...Array.from(new Set(focus1.data.map(d => d.focus1))),
+      default_selection,
+    ])
+    .join("option")
+    .attr("value", d => d)
+    .text(d => d);
+  // this ensures that the selected value is the same as what we have in state when we initialize the options
+  selectElement.property("value", default_selection);
+  
+  /** MAIN CODE */
+  svg = d3
+    .select("#d3-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+  
+    // add the xAxis
+  svg
+    .append("g")
+    .attr("class", "axis x-axis")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(xAxis)
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("x", "50%")
+    .attr("dy", "3em")
+    .text("Borough");
+  // add the yAxis
+  svg
+    .append("g")
+    .attr("class", "axis y-axis")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(yAxis)
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("y", "50%")
+    .attr("dx", "-3em")
+    .attr("writing-mode", "vertical-rl")
+    .text("Number of Organizations");
+  
+  const rect = svg
+    .selectAll("rect")
+    .data(filteredData, d => d.focus1) // use `d.name` as the `key` to match between HTML and data elements
+    .join("rect")
+    .attr("y", d => yScale(d.OrganizationNumber))
+    .attr("x", d => xScale(d.Borough))
+    .attr("width", xScale.bandwidth())
+    .attr("height", d => height - margin.bottom - yScale(d.count))
+    .attr("fill", "steelblue");
+  
+  draw();
 }
-
 /**
  * DRAW FUNCTION
  * we call this everytime there is an update to the data/Country
@@ -110,24 +120,6 @@ function draw() {
   // filter the data for the selectedLocation
   let filteredData = [];
   // if there is a selectedLocation, filter the data before mapping it to our elements
-  if (Country.selectedLocation !== null) {
-    filteredData = focus1.data.filter(d => d.focus1 === focus1.selectedFocus);
-  }
-
-// append rects
-const rect = svg
-  .selectAll("rect")
-  .data(filteredData, d => d.focus1)
-  .join("rect")
-  .attr("y", d => yScale(d.OrganizationNumber))
-  .attr("x", d => xScale(d.Borough))
-  .attr("width", xScale.bandwidth())
-  .attr("height", d => height - margin.bottom - yScale(d.OrganizationNumber))
-  .attr("fill", "steelblue")
-
-svg
-  .append("g")
-  .attr("class", "axis")
-  .attr("transform", `translate(0, ${height - margin.bottom})`)
-  .call(xAxis);
-  });
+  if (focus1.selectedFocus !== null) {
+    filteredData = focus1.data.filter(d => d.focus1 === selectedFocus);
+  }};
