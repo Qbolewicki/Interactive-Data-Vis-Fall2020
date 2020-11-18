9049295c3a8d4cb791aa8dc4f827707ce936bc63
Qbolewicki <Qbolewicki@icloud.com>
2020-11-18 10:30:00 -0500

    laptop edits

--- Womens_project/main.js
+++ Womens_project/main.js
@@ -5,6 +5,7 @@ const width = window.innerWidth * 0.7,
 height = window.innerHeight * 0.7,
   paddingInner = 0.2,
   margin = { top: 20, bottom: 50, left: 60, right: 40 },
+  radius = 5;
   default_selection = "Select a Service";
 
 let svg;
@@ -16,40 +17,46 @@ let yScale;
  * */
 let focus1 = {
   data: [],
-  selectedFocus: "All"
+  selectedFocus: "All",
 };
+
 /**
 *LOAD DATA
 **/
-d3.csv("../data/womens_resource.csv", d3.autoType)
+d3.csv("../data/womens_resource.csv", 
+d3.autoType)
 .then(raw_data => {
   console.log("raw_data", raw_data);
   focus1.data = raw_data;
 init();
 });
 
+//INITIALIZING FUNCTION
 function init() {
 
   /** SCALES */
-  // reference for d3.scales: https://github.com/d3/d3-scale
   xScale = d3
     .scaleBand()
-    .domain(d3.extent(focus1.data, d => d.Borough))
-    .range([margin.left, width - margin.right])
+    .domain(Array.from(new Set(focus1.data.map(d => d.Borough))))
+    .range([margin.left, width - margin.right]);
     .paddingInner(paddingInner);
+
+  console.log(Array.from(new Set(focus1.data.map(d => d.Borough))))
+
   yScale = d3
     .scaleLinear()
-    .domain(d3.extent(focus1.data, d => d.OrganizationNumber))
+    .domain(0, d3.max(focus1.data, d => d.OrganizationNumber))
     .range([height - margin.bottom, margin.top]);
-  
-  const xAxis = d3.axisBottom(xScale);
-  const yAxis = d3.axisLeft(yScale);
+ 
+// AXES
+const xAxis = d3.axisBottom(xScale);
+const yAxis = d3.axisLeft(yScale);
   
   // UI ELEMENT SETUP
   // add dropdown (HTML selection) for interaction
   // HTML select reference: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select
   const selectElement = d3.select("#dropdown").on("change", function() {
-    console.log("new selected Focus is", this.value);
+    console.log("new selected focus is", this.value);
     // `this` === the selectElement
     // this.value holds the dropdown value a user just selected
     focus1.selectedFocus = this.value;
@@ -66,6 +73,7 @@ function init() {
     .join("option")
     .attr("value", d => d)
     .text(d => d);
+    
   // this ensures that the selected value is the same as what we have in state when we initialize the options
   selectElement.property("value", default_selection);
   
@@ -76,30 +84,35 @@ function init() {
     .attr("width", width)
     .attr("height", height);
   
-    // add the xAxis
-  svg
-    .append("g")
-    .attr("class", "axis x-axis")
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(xAxis)
-    .append("text")
-    .attr("class", "axis-label")
-    .attr("x", "50%")
-    .attr("dy", "3em")
-    .text("Borough");
-  // add the yAxis
-  svg
-    .append("g")
-    .attr("class", "axis y-axis")
-    .attr("transform", `translate(${margin.left},0)`)
-    .call(yAxis)
-    .append("text")
-    .attr("class", "axis-label")
-    .attr("y", "50%")
-    .attr("dx", "-3em")
-    .attr("writing-mode", "vertical-rl")
-    .text("Number of Organizations");
+      // append rects
+  const rect = svg
+  .selectAll("rect")
+  .data(data)
+  .join("rect")
+  .attr("y", d => yScale(d.OrganizationNumber))
+  .attr("x", d => xScalex(d.Borough))
+  .attr("width", xScale.bandwidth())
+  .attr("height", d => height - margin.bottom - yScale(d.OrganizationNumber))
+  .attr("fill", "steelblue")
+
+// append text
+const text = svg
+  .selectAll("text")
+  .data(data)
+  .join("text")
+  .attr("class", "label")
+  // this allows us to position the text in the center of the bar
+  .attr("x", d => xScale(d.Borough) + (xScale.bandwidth() / 2))
+  .attr("y", d => yScale(d.OrganizationNumber))
+  .text(d => d.count)
+  .attr("dy", "1.25em");
   
+  svg
+    .append("g")
+    .attr("class", "axis")
+    .attr("transform", `translate(0, ${height - margin.bottom})`)
+    .call(xAxis);
+
   draw();
 }
 /**
@@ -108,48 +121,10 @@ function init() {
  * */
 function draw() {
   // filter the data for the selectedLocation
-  let filteredData = [];
+  let filteredData = focus1.data;
   // if there is a selectedLocation, filter the data before mapping it to our elements
-  if (focus1.selectedFocus !== null) {
-    filteredData = focus1.data.filter(d => d.focus1 === selectedFocus);
+  if (focus1.selectedFocus !== "All") {
+    filteredData = focus1.data.filter(d => d.Focus === focus1.selectedFocus);
   }
-  const rect = svg
-  .selectAll("rect")
-  .data(filteredData, d => d.focus1) // use `d.name` as the `key` to match between HTML and data elements
-  .join(
-    enter =>
-      // enter selections -- all data elements that don't have a `.dot` element attached to them yet
-      enter
-        .append("rect")
-        .attr("class", "rect") // Note: this is important so we can identify it in future updates
-        .attr("x", d => xScale(d.Borough))
-        .attr("y", d => margin.bottom) // initial value - to be transitioned
-        .call(enter =>
-          enter
-            .transition() // initialize transition
-            .delay(d => 0 * d.Borough) // delay on each element
-            .duration(1000) // duration 1000ms
-            .attr("y", d => yScale(d.OrganizationNumber))
-        ),
-    update =>
-      update.call(update =>
-        // update selections -- all data elements that match with a `.dot` element
-        update
-          .transition()
-          .duration(1000)
-          .transition()
-          .duration(250)
-          .attr("stroke", "lightgrey")
-      ),
-    exit =>
-      exit.call(exit =>
-        // exit selections -- all the `.dot` element that no longer match to HTML elements
-        exit
-          .transition()
-          .delay(d => 0 * d.Borough)
-          .duration(1000)
-          .attr("y", height - margin.bottom)
-          .remove()
-      )
-  );
+  
 }
